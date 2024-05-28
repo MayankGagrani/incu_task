1db7d2953f20d3a21fa8bf87f5bef2223fd05349
Mayank Gagrani <m.gagrani01@gmail.com>
2024-05-28 11:59:32 +0530

    code for  exception of negative numbers

--- app/services/incu_task_logic.rb
+++ app/services/incu_task_logic.rb
@@ -2,6 +2,13 @@ class IncuTaskLogic
 	def self.add(numbers_string)
 		return 0 if numbers_string.empty?
 		num = numbers_string.split(',').map(&:to_i) # sum of number in string
+
+		 #for checking negative numbers
+    if numbers_string.include?("-")
+      num = numbers_string.split(',').map(&:to_i).select { |num| num < 0 }.join(", ")
+      return "negative numbers not allowed #{num}"
+    end
+    
 		num.sum
 	end
 end
