522fc510cfe17879f62625b1acd8befb61fd76f4
Mayank Gagrani <m.gagrani01@gmail.com>
2024-05-28 11:38:30 +0530

    code for sigle digit string

--- app/services/incu_task_logic.rb
+++ app/services/incu_task_logic.rb
@@ -1,5 +1,7 @@
 class IncuTaskLogic
 	def self.add(numbers_string)
 		return 0 if numbers_string.empty?
+		num = numbers_string.split(',').map(&:to_i) # sum of number in string
+		num.sum
 	end
 end
